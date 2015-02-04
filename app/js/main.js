d4ee360a912328805695a87df89d2c2c6cb98081
thedean11 <edean1010@gmail.com>
2015-02-04 13:04:41 -0600

    added getjson functionality

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
-;(function(){
+(function(){
   'use strict';
+  var url = 'https://savingsmultipliedssh.firebaseio.com/items.json'
+  $.get(url, function(res){
+    _.forEach(res, function(item){
+        createList(item);
+    });
+  });
 
-  console.log('NSS Template Loaded');
+  function createList(data) {
+    var $li = $('<li></li>');
+      var $pic = $('<img class="images" src='+data.image+'></img>');
+      var $title = $('<h6>'+data.title+'</h6>');
+      var $seller = $('<p>'+data.seller+'</p>');
+      var $price = $('<p>'+data.price+'</p>');
+      var $endDate = $('<p>'+data.endDate+'</p>');
+    $li.append($pic);
+    $li.append($title);
+    $li.append($seller);
+    $li.append($price);
+    $li.append($endDate);
+    
+    var $searchResultsList = $('#searchResultsList');
+    $searchResultsList.append($li);
+  }
+
 }());
+
